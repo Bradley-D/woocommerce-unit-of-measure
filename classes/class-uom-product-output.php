8876fe85c580835c3bd1c06913a92396e743e12d
Bradley Davis <bradleydavis@Bradleys-MBP.BigPond>
2018-05-08 12:12:59 +1000

    [ UPDATE ] on conditional check for output

--- classes/class-uom-product-output.php
+++ classes/class-uom-product-output.php
@@ -18,25 +18,29 @@ class Woo_Uom_Output {
    * @since 1.0.1
    */
   function uom_add_actions_filters() {
-    add_filter( 'woocommerce_get_price_html', array( &$this, 'woo_uom_render_output' ) );
+    add_filter( 'woocommerce_get_price_html', array( &$this, 'woo_uom_render_output' ), 10, 2 );
+		//add_filter( 'woocommerce_cart_item_price', array( &$this, 'woo_uom_variation_output_cart' ), 10, 3 );
   }
 
-  /**
-   * Render the output
-   * @since 1.0.1
-   * @return $price + UOM string
-   */
-	 function woo_uom_render_output( $price ) {
-    global $woocommerce, $post;
-    // Check if uom_pro is installed
-    if ( in_array( 'wc-unit-measure-pro/wc-unit-measure-pro.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) :
-      return $price;
-    else :
-      // Display Custom Field Value
-      $woo_uom_output = get_post_meta( $post->ID, '_woo_uom_input', true );
-      return $price . ' ' . '<span class="uom">' . $woo_uom_output . '</span>';
-    endif;
-  }
+	/**
+	 * Render the output
+	 * @since 1.0.1
+	 * @return $price + UOM string
+	 */
+	 function woo_uom_render_output( $price, $product ) {
+	  global $woocommerce, $post;
+	  // Get product type
+	  $uom_product_type = $product->is_type( 'variable' );
+	  // Check if uom text exists
+	  $woo_uom_output = get_post_meta( $post->ID, '_woo_uom_input', true );
+	  // Check if variable OR UOM text exists
+	  if ( ( true === $uom_product_type ) || ( ! $woo_uom_output ) ) :
+	    return $price;
+	  else :
+	    $woo_uom_price_string = $price . ' ' . '<span class="uom">' . $woo_uom_output . '</span>';
+	    return $woo_uom_price_string;
+	  endif;
+	}
 
 }
 // Instantiate the class
